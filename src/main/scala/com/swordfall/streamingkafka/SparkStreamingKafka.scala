92c1a8243b394568f7e15dd2c1660a0580a5cb64
SwordfallYeung <2506597416@qq.com>
2019-09-10 20:58:27 +0800

    添加spark streaming kafka

--- src/main/scala/com/swordfall/streamingkafka/SparkStreamingKafka.scala
+++ src/main/scala/com/swordfall/streamingkafka/SparkStreamingKafka.scala
@@ -1,8 +1,14 @@
 package com.swordfall.streamingkafka
 
+import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
+import org.apache.spark.streaming.kafka010.{ConsumerStrategies, HasOffsetRanges, KafkaUtils, LocationStrategies}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.codehaus.jackson.map.deser.std.StringDeserializer
+import redis.clients.jedis.Pipeline
+import shaded.parquet.org.apache.thrift.Option.Some
 
 /**
   * 获取topic最小的offset
@@ -57,6 +63,59 @@ object SparkStreamingKafka {
     }
     InternalRedisClient.getPool.returnResource(jedis)
 
-    println("lastOffset")
+    println("lastOffset from redis -> " + lastOffset)
+
+    // 设置每个分区起始的Offset
+    val fromOffsets = Map{ new TopicPartition(topic, partition) -> lastOffset }
+
+    // 使用Direct API 创建Stream
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      LocationStrategies.PreferConsistent,
+      ConsumerStrategies.Assign[String, String](fromOffsets.keys.toList, kafkaParams, fromOffsets)
+    )
+
+    // 开始处理批次消息
+    stream.foreachRDD{
+      rdd =>
+        val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+        val result = processLogs(rdd)
+        println("================= Total " + result.length + " events in this batch ..")
+
+        val jedis = InternalRedisClient.getPool.getResource
+        val p1: Pipeline = jedis.pipelined()
+        p1.select(dbDefaultIndex)
+        p1.multi() // 开启事务
+
+        // 逐条处理消息
+        result.foreach{
+          record =>
+            // 增加小时总pv
+            val pv_by_hour_key = "pv" + record.hour
+            p1.incr(pv_by_hour_key)
+
+            // 增加网站小时pv
+        }
+    }
+  }
+
+  case class MyRecord(hour: String, user_id: String, site_id: String)
+
+  def processLogs(messages: RDD[ConsumerRecord[String, String]]): Array[MyRecord] = {
+    messages.map(_.value()).flatMap(parseLog).collect()
+  }
+
+  def parseLog(line: String): Option[MyRecord] = {
+    val ary: Array[String] = line.split("\\|~\\|", -1);
+    try{
+      val hour = ary(0).substring(0, 13).replace("T", "-")
+      val uri = ary(2).split("[=|&", -1)
+      val user_id = uri(1)
+      val site_id = uri(3)
+      return scala.Some(MyRecord(hour, user_id, site_id))
+    }catch{
+      case ex: Exception => println(ex.getMessage)
+    }
+    return None
   }
 }
